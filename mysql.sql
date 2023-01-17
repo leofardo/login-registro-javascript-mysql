c681c71b357d4bb7e9d1f5089d36572f8c31b383
Leonardo Ferreira <69652528+leofardo@users.noreply.github.com>
2023-01-17 00:04:22 -0300

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE db_login_registro;
 
 CREATE TABLE `db_login_registro`.`usuarios` (
-    `idusuarios` INT NOT NULL,
+    `idusuarios` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `nome` VARCHAR(30) NOT NULL,
     `email` VARCHAR(45) NOT NULL,
     `password` VARCHAR(200) NOT NULL,
